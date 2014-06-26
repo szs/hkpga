9a8553975ca61409de0db619eb47b758c46a5a65
Mart van de Ven <m@type.hk>
2014-06-26 21:56:24 +0800

    Placeholder template files

--- app/scripts/translations.js
+++ app/scripts/translations.js
@@ -1,6 +1,6 @@
 var translationsEN = {
   MENU_HOME: 'Home',
-  MENU_PROFESSIONALS: 'News',
+  MENU_PROFESSIONALS: 'Professionals',
   MENU_TOURNAMENTS: 'Tournaments',
   MENU_MEMBERS: 'Members',
   MENU_JUNIORS: 'Juniors',
