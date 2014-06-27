21a39a64313dae18a1ce0543765529721b4d09d6
Sherin Siew <sz.sherin@gmail.com>
2014-06-28 02:06:42 +0800

    Adds and styles two-level navbar. Footer styling.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,3 +9,31 @@ app.controller('MainCtrl', function($scope){
 });
  
 
+
+$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+  // Avoid following the href location when clicking
+  event.preventDefault();
+  // Avoid having the menu to close when clicking
+  event.stopPropagation();
+  // Toggle menu.
+  if ($(this).parent().hasClass('open'))
+    $(this).parent().removeClass('open');
+  else
+    $(this).parent().addClass('open');
+  // If children are open, close them.
+  $(this).children().removeClass('open');
+  // If a sibling menu is already open we close it
+  $(this).parent().siblings().removeClass('open').children().removeClass('open');
+  $(this).siblings().children().removeClass('open');
+  
+  var menu = $(this).parent().find("ul");
+  var menupos = menu.offset();
+  
+  if (menupos.left + menu.width() > $(window).width()) {
+    var newpos = -$(menu).width();
+    menu.css({ left: newpos });
+  } else {
+    var newpos = $(this).parent().width();
+    menu.css({ left: newpos });
+  }
+});
