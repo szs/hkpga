aaf764956380763c29ff1ce088ca0d6bb737e6cb
Sherin Siew <sz.sherin@gmail.com>
2014-07-04 04:57:26 +0800

    Added sidenav to committee and fixed responsiveness.

--- app/scripts/translations.js
+++ app/scripts/translations.js
@@ -43,6 +43,7 @@ var translationsEN = {
   MENS_SENIOR_DIVISION: 'Men\'s Senior Division',
   BACK: 'Back',
   LOGIN: 'Login',
+  OFFICIAL_PARTNERS: 'Official Partners',
   EVENTS: 'Events',
   MAGAZINE: 'Magazine',
   VIEW_MAGAZINES: 'See all magazines',
